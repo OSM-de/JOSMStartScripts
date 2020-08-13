79984cc00c78fba4cee7788cc4c896df6ca3d5fd
Sören Reinecke <valinora@gmx.net>
2020-08-13 15:55:31 +0200

    HTTP --> HTTPS

--- addjosm.sh
+++ addjosm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-wget -q -O "-" http://josm.openstreetmap.de/josm-apt.key | apt-key add - 
-echo "deb http://josm.openstreetmap.de/apt alldist universe" > /etc/apt/sources.list.d/josm.list
+wget -q -O "-" https://josm.openstreetmap.de/josm-apt.key | apt-key add - 
+echo "deb https://josm.openstreetmap.de/apt alldist universe" > /etc/apt/sources.list.d/josm.list
 apt update
 apt install josm
 
